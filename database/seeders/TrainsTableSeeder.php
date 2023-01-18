3033313bf66b907c43bf04caf7a40e01d8bc7ef7
dav100ide <oreglio.dev@gmail.com>
2023-01-18 19:22:56 +0100

    ho seedato il database

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -15,20 +15,22 @@ class TrainsTableSeeder extends Seeder
      * @return void
      */
     public function run(Faker $faker)
+
     {
+        for ($i = 0; $i < 10; $i++) {
+            $new_train = new Train(); // istanzio un ogg del modello
 
-        $new_train = new Train(); // istanzio un ogg del modello
+            $new_train->company = $faker->randomElement(['Italo', 'Trenord', 'Trenitalia', 'Eurocity']);
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->date();
+            $new_train->arrival_time = $faker->date();
+            $new_train->train_code = $faker->postcode();
+            $new_train->wagons = rand(3, 8);
+            $new_train->on_time = rand(0, 1);
+            $new_train->cancelled = rand(0, 1);
 
-        $new_train->company = $faker->randomElement(['Italo', 'Trenord', 'Trenitalia', 'Eurocity']);
-        $new_train->departure_staion = $faker->city();
-        $new_train->arrival_station = $faker->city();
-        $new_train->departure_time = $faker->time();
-        $new_train->arrival_time = $faker->time();
-        $new_train->train_code = $faker->postcode();
-        $new_train->wagons = rand(3, 8);
-        $new_train->on_time = rand(0, 1);
-        $new_train->cancelled = rand(0, 1);
-
-        $new_train->save();
+            $new_train->save();
+        }
     }
 }
